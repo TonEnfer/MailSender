14f2c2ffefb95d1b8a5914761ad732a25713eb94
TonEnfer <tonik.sam@gmail.com>
2018-04-15 19:45:54 +0500

    init

---
+++ MailSender/MsgBilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MailSender
+{
+    public static class MsgFileBilder
+    {
+        struct Parameters
+        {
+            string name;
+            string value;
+        }
+
+        static void Build(Parameters[] param, string patch) {
+
+            throw new InvalidArgumentFormat();
+        }
+    }
+
+    [Serializable()]
+    public class InvalidArgumentFormat : System.ArgumentException
+    {
+        public InvalidArgumentFormat() : base() { }
+        public InvalidArgumentFormat(string message) : base(message) { }
+        public InvalidArgumentFormat(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected InvalidArgumentFormat(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
+}
---
+++ MailSender/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = Declator.Decline("Иванов Иван Иванович", NameCaseLib.NCL.Padeg.RODITLN);
+        }
+    }
+}
